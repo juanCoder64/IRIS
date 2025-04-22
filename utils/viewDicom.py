ec1cc73fb8ea563fd3ae1b940cdbbfe93e0288c0
juanCoder <jgama12569@gmail.com>
2025-04-22 13:15:52 -0600

    el frontend

--- utils/viewDicom.py
+++ utils/viewDicom.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # puse los dicoms adentro del proyecto
 # les recomiendo hacer lo mismo, el git está configurado
 # para ignorar la carpeta DICOMS, entonces no se van a subir
-path = '../DICOMS/Rayos X/azc201047013'
+path = '../DICOMS/Mastografias/ CAN912724033'
 
 ds= dcmread(path+'/DICOMDIR')
 
@@ -17,8 +17,15 @@ for r in ds.DirectoryRecordSequence: #por cada registro en el dicomdir
         estudio= pydicom.dcmread(os.path.join(path+'/'+r.ReferencedFileID[0]+'/'+r.ReferencedFileID[1]))
         if hasattr(estudio,'pixel_array'):
             # si es imagen la muestra
+            plt.axis('off')
+            plt.subplots_adjust(0,0,1,1)
             plt.imshow(estudio.pixel_array, cmap='gray')
-            plt.show()
+            #plt.show()
+            #save image as jpg with the same name as the dicom
+            plt.imsave(os.path.join(estudio.SOPInstanceUID+'.jpg'),estudio.pixel_array, cmap='gray')
+
+
+
         else:
             # si no es imagen imprime la información
             print(estudio)
