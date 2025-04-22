7a3cce1fcf1a7bc199506b98f73cbd3fc0d5fb39
juanCoder <jgama12569@gmail.com>
2025-04-22 13:23:09 -0600

    el frontend

---
+++ Viewers-3.10.1/platform/app/public/config/docker-nginx-orthanc.js
@@ -0,0 +1,73 @@
+/** @type {AppTypes.Config} */
+window.config = {
+  routerBasename: null,
+  showStudyList: true,
+  extensions: [],
+  modes: [],
+  // below flag is for performance reasons, but it might not work for all servers
+  whiteLabeling: {
+    createLogoComponentFn: function (React) {
+      return React.createElement(
+        'a',
+        {
+          target: '_blank',
+          rel: 'noopener noreferrer',
+          className: 'text-white underline',
+          href: 'http://radicalimaging.com',
+        },
+        React.createElement('img', {
+          src: '/assets/LogoSaludDigna.svg',
+        })
+      );
+    },
+  },
+  investigationalUseDialog: 'never',
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  experimentalStudyBrowserSort: false,
+  strictZSpacingForVolumeViewport: true,
+  studyPrefetcher: {
+    enabled: true,
+    displaySetsCount: 2,
+    maxNumPrefetchRequests: 10,
+    order: 'closest',
+  },
+  defaultDataSourceName: 'dicomweb',
+  dataSources: [
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
+        friendlyName: 'Orthanc Server',
+        name: 'Orthanc',
+        wadoUriRoot: '/wado',
+        qidoRoot: '/pacs/dicom-web',
+        wadoRoot: '/pacs/dicom-web',
+        qidoSupportsIncludeField: false,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        dicomUploadEnabled: true,
+        omitQuotationForMultipartRequest: true,
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
+      sourceName: 'dicomjson',
+      configuration: {
+        friendlyName: 'dicom json',
+        name: 'json',
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
+      sourceName: 'dicomlocal',
+      configuration: {
+        friendlyName: 'dicom local',
+      },
+    },
+  ],
+  httpErrorHandler: error => {
+    console.warn(`HTTP Error Handler (status: ${error.status})`, error);
+  },
+};
